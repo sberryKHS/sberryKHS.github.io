1a03ee95ab7284c885e4add4b428f5d2e0b23bb2
sberryKHS <132116397+sberryKHS@users.noreply.github.com>
2026-04-29 13:10:09 +0000

    larp

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -98,14 +98,15 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if (circle.x > canvas.width) {
-                circle.x = 0;
-            } else if (circle.x < 0) {
+            else if (circle.x < 0) {
                 circle.x = canvas.width;
             }
+            //BOTTOM SIDE
             if (circle.y > canvas.height) {
                 circle.y = 0;
-            } else if (circle.y < 0) {
+            }
+            //TOP SIDE
+            else if (circle.y < 0) {
                 circle.y = canvas.height;
             }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
